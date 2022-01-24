a80d0d1af00487b1acf69ef7772e40aafd66f504
Daniel Christian <danielscottchristian@gmail.com>
2022-01-23 19:11:19 -0600

    More changes to count, tests, animations, etc.

---
+++ public/homepage.js
@@ -0,0 +1,3 @@
+gsap.fromTo(".landing-text", {opacity: 0, y:50}, {duration: 0.75, opacity: 1, y:0});
+gsap.fromTo("img", {opacity: 0, y:50}, {duration: 0.75, opacity: 1, y:0});
+gsap.fromTo(".nav-link", {opacity: 0}, {duration: 1, opacity: 1});
