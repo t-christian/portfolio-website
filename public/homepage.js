7199a4459c8967ca6432747df3f45d5b62f889d3
Daniel Christian <danielscottchristian@gmail.com>
2022-01-23 20:47:47 -0600

    lots of changes, animations mostly and redo of contact page

--- public/homepage.js
+++ public/homepage.js
@@ -1,3 +1,12 @@
-gsap.fromTo(".landing-text", {opacity: 0, y:50}, {duration: 0.75, opacity: 1, y:0});
+gsap.fromTo(".animateUp", {opacity: 0, y:50}, {duration: 0.75, opacity: 1, y:0});
 gsap.fromTo("img", {opacity: 0, y:50}, {duration: 0.75, opacity: 1, y:0});
 gsap.fromTo(".nav-link", {opacity: 0}, {duration: 1, opacity: 1});
+
+
+function scaleUp(id) {
+    gsap.to("#" + id, {scale: 1.1, duration: 0.5});
+}
+
+function scaleDown(id) {
+    gsap.to("#" + id, {scale: 1, duration: 0.5});
+}
