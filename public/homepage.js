37febed6c2b251ecde99ba4e8b8697dd35b1614c
Daniel Christian <danielscottchristian@gmail.com>
2022-02-04 22:42:21 -0600

    set cookie for nav bar fade in

--- public/homepage.js
+++ public/homepage.js
@@ -1,7 +1,15 @@
+var x = document.cookie
+
+if (x=null) {
+    console.log("fade in")
+    gsap.fromTo(".nav-link", {opacity: 0}, {duration: 1, opacity: 1});
+    }
+else{
+    console.log("no fade in")
+}
+
 gsap.fromTo(".animateUp", {opacity: 0, y:50}, {duration: 0.75, opacity: 1, y:0});
 gsap.fromTo("img", {opacity: 0, y:50}, {duration: 0.75, opacity: 1, y:0});
-gsap.fromTo(".nav-link", {opacity: 0}, {duration: 1, opacity: 1});
-
 
 function scaleUp(id) {
     gsap.to("#" + id, {scale: 1.1, duration: 0.5});
@@ -10,3 +18,8 @@ function scaleUp(id) {
 function scaleDown(id) {
     gsap.to("#" + id, {scale: 1, duration: 0.5});
 }
+
+
+//set cookie for nav bar fade in
+console.log('setting cookie')
+document.cookie = 'visited: true; path:/'
