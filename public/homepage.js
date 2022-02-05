e619a4770d6e35d1338463e40359b31712f391ef
Daniel Christian <danielscottchristian@gmail.com>
2022-02-04 22:56:04 -0600

    fix cookie 2

--- public/homepage.js
+++ public/homepage.js
@@ -1,14 +1,14 @@
-var x = document.cookie
 
-if (x=null) {
+//visited cookie logic
+if (document.cookie=='') {
     gsap.fromTo(".nav-link", {opacity: 0}, {duration: 1, opacity: 1});
     document.cookie = 'visited: true; path:/'
     }
-
 else{
     gsap.fromTo(".nav-link", {opacity: 1}, {duration: 1, opacity: 1});
 }
 
+//animation logic
 gsap.fromTo(".animateUp", {opacity: 0, y:50}, {duration: 0.75, opacity: 1, y:0});
 gsap.fromTo("img", {opacity: 0, y:50}, {duration: 0.75, opacity: 1, y:0});
 
