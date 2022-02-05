fbe8c44cc598d5e100624121071268ed0616316f
Daniel Christian <danielscottchristian@gmail.com>
2022-02-04 22:46:27 -0600

    cookie fix

--- public/homepage.js
+++ public/homepage.js
@@ -1,11 +1,12 @@
 var x = document.cookie
 
 if (x=null) {
-    console.log("fade in")
     gsap.fromTo(".nav-link", {opacity: 0}, {duration: 1, opacity: 1});
+    document.cookie = 'visited: true; path:/'
     }
+
 else{
-    console.log("no fade in")
+    gsap.fromTo(".nav-link", {opacity: 1}, {duration: 1, opacity: 1});
 }
 
 gsap.fromTo(".animateUp", {opacity: 0, y:50}, {duration: 0.75, opacity: 1, y:0});
@@ -18,8 +19,3 @@ function scaleUp(id) {
 function scaleDown(id) {
     gsap.to("#" + id, {scale: 1, duration: 0.5});
 }
-
-
-//set cookie for nav bar fade in
-console.log('setting cookie')
-document.cookie = 'visited: true; path:/'
